e337030f15f0d9ca4fe82752fad4a469d98585c5
bill.yan <yan_zhuanshu@163.com>
2021-07-27 16:08:43 +0800

    feat: add /

--- src/api/index.js
+++ src/api/index.js
@@ -4,7 +4,7 @@ const fetch = api.http
 
 export function getUserInfo (params) {
     return fetch({
-        url: 'vue-study/user',
+        url: '/vue-study/user',
         method: get,
         params,
     })
