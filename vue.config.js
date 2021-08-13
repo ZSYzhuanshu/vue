518b81c45bdedfe025d1914edef24668b5750428
bill.yan <yan_zhuanshu@163.com>
2021-08-13 19:00:43 +0800

    feat: add webpack-bundle-analyzer

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 let entry = { entry: './src/main.js' }
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
     pages: {
@@ -7,10 +8,14 @@ module.exports = {
     },
     outputDir: 'dist',
     devServer: {
-      overlay: {
-        warnings: false, //不显示警告
-        errors: false	//不显示错误
-      },
+      // overlay: {
+      //   warnings: false, //不显示警告
+      //   errors: false	//不显示错误
+      // },
     },
-    lintOnSave: true, //关闭eslint检查
+    // lintOnSave: true, //关闭eslint检查
+    chainWebpack: config => {
+      config.plugin('webpack-bundle-analyzer')
+        .use(BundleAnalyzerPlugin)
+    }
 }  
